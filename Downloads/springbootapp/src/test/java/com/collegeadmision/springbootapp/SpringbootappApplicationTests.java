02c3487eb7afc3f799f22bcd43dd680fda705f33
Hari Nale <harinale132@gmail.com>
2024-02-26 22:53:07 +0530

    WIP

---
+++ Downloads/springbootapp/src/test/java/com/collegeadmision/springbootapp/SpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeadmision.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
