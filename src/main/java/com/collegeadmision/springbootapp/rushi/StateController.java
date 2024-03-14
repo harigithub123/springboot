de9ea3f8bf56ae83f2c3b6726def8cae8cc7e90a
Rushi1-jgt <153620383+Rushi1-jgt@users.noreply.github.com>
2024-03-14 16:40:22 +0530

    commit

---
+++ src/main/java/com/collegeadmision/springbootapp/rushi/StateController.java
@@ -0,0 +1,16 @@
+package com.collegeadmision.springbootapp.rushi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class StateController {
+    @GetMapping(path = "ListofStatesInTheWorld")
+    public List<String> getStates() {
+        return Arrays.asList("BIHAR", "MAHARASHTRA","JHARKHAND");
+    }
+
+}
