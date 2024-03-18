d0da2656b43af11cd9cd8aa8ff5438cb28935752
Rushi1-jgt <153620383+Rushi1-jgt@users.noreply.github.com>
2024-03-18 21:53:43 +0530

    new update error

---
+++ src/main/java/com/collegeadmision/springbootapp/rushi/StudentRj.java
@@ -0,0 +1,36 @@
+package com.collegeadmision.springbootapp.rushi;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentRj {
+        private Long id;
+        @NotNull(message = "Value cannot be null")
+        private String name;
+        @Min(value = 1)
+        @Max(value = 150)
+        private int rollNo;
+        private String emailId;
+        @NotBlank
+        @Pattern(regexp = "[0-9]+")
+        @Size(min = 6, max = 6, message = "Pin code size must be 6 characters.")
+        private String pinCode;
+
+        @Override
+        public String toString() {
+            return "StudentRj{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", rollNo=" + rollNo +
+                    ", emailId='" + emailId + '\'' +
+                    '}';
+        }
+
+    }
